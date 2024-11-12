877aee447ebd7ce7f10d1a3d5be933640b439ba1
Raderwet <Raderwet@yandex.com>
2024-11-12 14:33:00 +0300

    GitTest

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Oh, hi, Mark!");
+            Console.WriteLine("WAARGGGHHHH!!!!");
             Console.ReadKey();
         }
     }
